d6ab500fd3a415f90e34337803bfdf528f217f01
Graphic <graphic@SOD-SJH-516-22M.local>
2015-11-27 14:21:58 -0500

    target and points

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 var position = 0;
 var dir = 1;
 var rotate = function() {
-	console.log(position);
+	//console.log(position);
 	position += 1 * dir;
 	$('.dial').css({'transform':'rotateZ('+position+'deg)'});
 };
 
-setInterval(rotate, 12);
+timer = setInterval(rotate, 12);
 
 var numofclicks = 0;
 
-	$('body').on('keyup', function(e){
+	$('body').on('keydown', function(e){
 		console.log(e.keyCode)
 		if(e.keyCode == 32){
-			dir *= -1;
+			if (Math.abs(position % 360) >= 356 || Math.abs(position % 360) <= 5) {
+				console.log(Math.abs(position % 360), "YES")
+				dir *= -1;
+
+				numofclicks = (numofclicks + 1);
+				$('.count').text(numofclicks);
+			}
+			else {
+				console.log(Math.abs(position % 360), "NO")
+				dir = 0;
+				alert("YOU LOSE!");
+			}
+
+			
 		};
 
-	numofclicks = (numofclicks + 1);
-
-	$('.count').text(numofclicks);
 
 
 	});
-
-
-// CANT FIGURE OUT HOW TO RANDOMIZE DOTS
 
