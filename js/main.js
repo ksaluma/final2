7ded4e177c6cbf66614afdb05feec02af494826b
ksaluma <kristen.saluma@hotmail.com>
2015-12-04 01:47:12 -0500

    levels working, homescreen/replay

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ var rotate = function() {
 
 timer = setInterval(rotate, 12);
 
+
+// var hscore = 0;
 var numofclicks = 0;
+var lives= 3;
+
 
 	$('body').on('keydown', function(e){
 		console.log(e.keyCode)
@@ -18,16 +22,42 @@ var numofclicks = 0;
 				dir *= -1;
 
 				numofclicks = (numofclicks + 1);
-				$('.count').text(numofclicks);
-			}
-			else {
-				console.log(Math.abs(position % 360), "NO")
-				dir = 0;
-				alert("YOU LOSE!");
+				$('#count').text(numofclicks);
 			}
 
+			else {
+				console.log(Math.abs(position % 360), "NO")
+				// dir = 0;
+				// alert("YOU LOSE!");
+				// window.location.replace("index2.html");
+
+				lives = (lives - 1);
+				$('#lives').text(lives);
+			}
+
+			if (lives <1) {
+				// alert("YOU LOSE!");
+				window.location.replace("index2.html");
+			}
 			
 		};
+
+	// if (numofclicks >) 
+	// 	hscore = (numofclicks + 1);
+
+// $('.hscore').text(hscore);
+
+	// function logScore(score) {
+ //    if (score > localStorage.getItem('score'))
+ //      localStorage.setItem('score', score);
+ //    $('.score span.count').text(score);
+ //  }
+ //  loadScore()
+
+ //  function loadScore() {
+ //    $('.score span.hscore').text(localStorage.getItem('score') ? localStorage.getItem('score') : 0);
+ //    $('.score span.count').text(0);
+ //  }
 
 
 
